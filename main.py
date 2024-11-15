7e23ae4521ccbac09ea2ea4b959a8d1d3ea6c538
hetbhalani <bhalanihet2006@gmail.com>
2024-11-15 20:39:42 +0530

    board

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 
 def makeBoard():
-    print(f' 0 | 1 | 2 ')
+    print(f' {text_color(0)} | {text_color(1)} | {text_color(2)} ')
     print(f'———|———|———')
-    print(f' 3 | 4 | 5 ')
+    print(f' {text_color(3)} | {text_color(4)} | {text_color(5)} ')
     print(f'———|———|———')
-    print(f' 6 | 7 | 8 ')
+    print(f' {text_color(6)} | {text_color(7)} | {text_color(8)} ')
 
-     
+def text_color(num):
+    return f'\033[97m{num}'
+        
 if __name__ == "__main__":
     o = [0] *9 
     x = [0] *9
