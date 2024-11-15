ff57ba1cd5685526c5ae885563188fa2d85240ee
hetbhalani <bhalanihet2006@gmail.com>
2024-11-15 22:38:24 +0530

    fix box

--- main.py
+++ main.py
@@ -9,11 +9,12 @@ def makeBoard(o,x):
             'O' if o[6]== 1 else ("X" if x[6] == 1 else 6),
             'O' if o[7]== 1 else ("X" if x[7] == 1 else 7),
             'O' if o[8]== 1 else ("X" if x[8] == 1 else 8)]
-    print(f' {colored(mark[0], "grey")} {colored('|', "magenta")} {colored(mark[1], "grey")} {colored('|', "magenta")} {colored(mark[2], "grey")} ')
+    
+    print(f' {colored(mark[0], "grey" if mark[0] == 0 else ("cyan" if mark[0]=="O" else "red"))} {colored('|', "magenta")} {colored(mark[1], "grey" if mark[1] == 1 else ("cyan" if mark[1] == "O" else "red"))} {colored('|', "magenta")} {colored(mark[2], "grey" if mark[2] == 2 else ("cyan" if mark[2]=="O" else "red"))} ')
     print(f'{colored('———', "magenta")}{colored('|', "magenta")}{colored('———', "magenta")}{colored('|', "magenta")}{colored('———', "magenta")}')
-    print(f' {colored(mark[3], "grey")} {colored('|', "magenta")} {colored(mark[4], "grey")} {colored('|', "magenta")} {colored(mark[5], "grey")} ')
+    print(f' {colored(mark[3], "grey" if mark[3] == 3 else ("cyan" if mark[3]=="O" else "red"))} {colored('|', "magenta")} {colored(mark[4], "grey" if mark[4] == 4 else ("cyan" if mark[4]=="O" else "red"))} {colored('|', "magenta")} {colored(mark[5], "grey" if mark[5] == 5 else ("cyan" if mark[5]=="O" else "red"))} ')
     print(f'{colored('———', "magenta")}{colored('|', "magenta")}{colored('———', "magenta")}{colored('|', "magenta")}{colored('———', "magenta")}')
-    print(f' {colored(mark[6], "grey")} {colored('|', "magenta")} {colored(mark[7], "grey")} {colored('|', "magenta")} {colored(mark[8], "grey")} ')
+    print(f' {colored(mark[6], "grey" if mark[6] == 6 else ("cyan" if mark[6]=="O" else "red"))} {colored('|', "magenta")} {colored(mark[7], "grey" if mark[7] == 7 else ("cyan" if mark[7]=="O" else "red"))} {colored('|', "magenta")} {colored(mark[8], "grey" if mark[8] == 8 else ("cyan" if mark[8]=="O" else "red"))} ')
     
 # def fun(x,o):
 #     for val in x:
