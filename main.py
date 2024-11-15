3ed7cf4b29efb599e1f4d8ccdf24d12a11132a52
hetbhalani <bhalanihet2006@gmail.com>
2024-11-15 20:33:03 +0530

    making this up

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("hello python")
+
+def makeBoard():
+    print(f' 0 | 1 | 2 ')
+    print(f'———|———|———')
+    print(f' 3 | 4 | 5 ')
+    print(f'———|———|———')
+    print(f' 6 | 7 | 8 ')
+
+     
+if __name__ == "__main__":
+    o = [0] *9 
+    x = [0] *9
+    turn = 0 
+    makeBoard()
+    
+     
