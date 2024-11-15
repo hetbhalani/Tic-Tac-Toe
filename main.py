f382fab4cdf7b1004709c8a96d965be711e032c3
hetbhalani <bhalanihet2006@gmail.com>
2024-11-15 20:47:59 +0530

    a

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-
+from termcolor import colored
 def makeBoard():
-    print(f' {text_color(0)} | {text_color(1)} | {text_color(2)} ')
-    print(f'———|———|———')
-    print(f' {text_color(3)} | {text_color(4)} | {text_color(5)} ')
-    print(f'———|———|———')
-    print(f' {text_color(6)} | {text_color(7)} | {text_color(8)} ')
-
-def text_color(num):
-    return f'\033[97m{num}'
+    print(f' {colored(0, "grey")} {colored('|', "magenta")} {colored(1, "grey")} {colored('|', "magenta")} {colored(2, "grey")} ')
+    print(f'{colored('———', "magenta")}{colored('|', "magenta")}{colored('———', "magenta")}{colored('|', "magenta")}{colored('———', "magenta")}')
+    print(f' {colored(3, "grey")} {colored('|', "magenta")} {colored(4, "grey")} {colored('|', "magenta")} {colored(5, "grey")} ')
+    print(f'{colored('———', "magenta")}{colored('|', "magenta")}{colored('———', "magenta")}{colored('|', "magenta")}{colored('———', "magenta")}')
+    print(f' {colored(6, "grey")} {colored('|', "magenta")} {colored(7, "grey")} {colored('|', "magenta")} {colored(8, "grey")} ')
+    
+# def text_color(num):
+#     return f'\033[97m{num}'
         
 if __name__ == "__main__":
     o = [0] *9 
